d30ac5a8a8db5005ec556ab5df7d5e12b80cd41d
machao <machao0605@qq.com>
2020-04-25 15:19:19 +0800

    25

---
+++ src/Group.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Group {
+    public static void main(String[] args) {
+        Random rd  = new Random();
+        int count = rd.nextInt(9999);
+        for (int i = 0;i<count;i++){
+            System.out.println("好好生活,好好学习,家人健康,身体健康");
+        }
+    }
+}
