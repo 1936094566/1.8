8b3fdf9cbed470725d0dacc6d3b4e70df9dfe4c7
machao <machao0605@qq.com>
2020-08-16 21:43:15 +0800

    816

--- src/Group.java
+++ src/Group.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Group {
     public static void main(String[] args) {
         Random rd  = new Random();
-        int count = rd.nextInt(9999);
+        int count = rd.nextInt(8888);
         for (int i = 0;i<count;i++){
             System.out.println("好好生活,好好学习,家人健康,身体健康");
             System.out.println("好好生活,好好学习,家人健康,身体健康");
