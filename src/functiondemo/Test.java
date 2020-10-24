1d1f85b035a5f3c7e1fc5e99b5aef165afba745d
machao <machao0605@qq.com>
2020-10-24 22:01:08 +0800

    1024

--- src/functiondemo/Test.java
+++ src/functiondemo/Test.java
@@ -7,6 +7,27 @@ package functiondemo;
  * @description
  **/
 public class Test {
+    public static Integer num = 0;
+
     public static void main(String[] args) {
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                addNum();
+            }
+        }).start();
+
+
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                addNum();
+            }
+        }).start();
+
     }
+
+    public synchronized  static void addNum() {
+        num++;
+        System.out.println("线程"+Thread.currentThread().getName()+",当前数字值"+num);
+    }
+
 }
